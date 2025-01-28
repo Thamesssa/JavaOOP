07cc348ef2c823a9450eee9befbb495427e31a9f
Thamesssa <tummysanqa@gmail.com>
2025-01-28 13:56:40 +0200

    Enhance Main class to add random numbers to both OddArrayList and standard ArrayList; improve output display

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
+public class Main {
+    public static void main(String[] args) {
+        OddArrayList oddList = new OddArrayList(1, 2, 3, 4, 7, 9, -13);
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 7, 9, -13));
 
-public static void main(String[] args) {
- 
-   OddArrayList oddList = new OddArrayList(1,2,3,4,7,9,-13);
-   System.out.println(oddList);
-   System.out.println("========================");
+        addRandomNumber(oddList);
+        addRandomNumber(list);
 
+        System.out.println("OddList: " + oddList);
+        System.out.println("List: " + list);
+    }
+
+    public static void addRandomNumber(ArrayList<Integer> list) {
+        Random random = new Random();
+        int originalSize = list.size();
+        while (list.size() == originalSize) {
+            int randomNumber = random.nextInt(1000); // Generate a random number between 0 and 999
+            list.add(randomNumber);
+        }
+    }
 }
