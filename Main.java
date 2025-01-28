c52bbee571a29890bd758dbb7702cf9808e25596
Thamesssa <tummysanqa@gmail.com>
2025-01-28 13:40:09 +0200

    Implement OddArrayList for filtering and managing odd integers; remove ModArrayList

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
+
+
 public static void main(String[] args) {
  
-    ModArrayList person1 = new ModArrayList();
-
-    person1.add("John");   
-    person1.add("Doe");
-    person1.add("Smith");
-
-    System.out.println(person1.getUsingMod(1000256)); // John
+   OddArrayList oddList = new OddArrayList(1,2,3,4,7,9,-13);
+   System.out.println(oddList);
+   System.out.println("========================");
 
 }
